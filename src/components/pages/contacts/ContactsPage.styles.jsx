97977a7240ea6420d797479b29f8dacae0556071
urraccon <133591634+urraccon@users.noreply.github.com>
2024-05-09 22:02:37 +0300

    fixed an error in the file path

--- src/components/pages/contacts/ContactsPage.styles.jsx
+++ src/components/pages/contacts/ContactsPage.styles.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100vh;
+  height: inherit;
   display: flex;
   justify-content: center;
   align-items: center;
